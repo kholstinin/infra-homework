5d89b77d45c51165a5a537a7c29371c94e63045a
Yaroslav Grinevich <yaroslav.grinevich@dsr-corporation.com>
2024-12-31 13:08:18 +0300

    [HW-1] Import *.yaml files

--- build/loader.cjs
+++ build/loader.cjs
@@ -1,2 +1,11 @@
 const yaml = require('yaml');
 const fs = require('node:fs');
+
+require.extensions['.yml'] = (module, filename) => {
+  try {
+    const content = fs.readFileSync(filename, 'utf8');
+    module.exports = yaml.parse(content);
+  } catch (e) {
+    console.error(`Failed to load ${filename}:\n`, e);
+  }
+}
--- build/loader.mjs
+++ build/loader.mjs
@@ -1,3 +1,23 @@
 import yaml from 'yaml';
 import path from 'node:path';
 import fs from 'node:fs/promises';
+
+export async function load(url, context, nextLoad) {
+  if (!isYaml(url)) return nextLoad(url, context);
+
+  try {
+    const content = await fs.readFile(new URL(url), 'utf8');
+    return {
+      shortCircuit: true,
+      format: 'json',
+      source: JSON.stringify(yaml.parse(content))
+    };
+  } catch (e) {
+    console.error(`Failed to load ${url}:\n`, e);
+  }
+}
+
+function isYaml(url) {
+  const ext = path.extname(url);
+  return ext === '.yml' || ext === '.yaml';
+}
